5122271e1b682362ace921e25bd610bab3ccd43e
Steven E. Harris <seh@panix.com>
2023-07-07 18:49:00 -0400

    Include basic nix-darwin bootstrapping application

---
+++ nix/packages/default.nix
@@ -0,0 +1,20 @@
+# Basis of inspiration:
+#   https://github.com/midchildan/dotfiles/blob/main/nix/packages/default.nix
+{ pkgs ? import <nixpkgs> { }
+, nixos ? import <nixos> { }
+, inputs ? { }
+}:
+
+let
+  inherit (pkgs.lib) optionalAttrs;
+  inherit (pkgs.stdenv) isDarwin;
+  nix = pkgs.nixVersions.${inputs.self.lib.config.nix.package};
+in
+{
+  # TODO(seh): Define common attributes.
+} // optionalAttrs isDarwin {
+  nix-darwin = pkgs.callPackage ./nix-darwin.nix {
+    inherit nix;
+    inherit (inputs) darwin;
+  };
+}
