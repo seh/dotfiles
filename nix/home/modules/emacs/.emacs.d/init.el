51b67661ed59fbc41a2e9c2e7f8cd13a18a02907
Steven E. Harris <seh@panix.com>
2023-07-07 11:32:02 -0400

    Move Emacs customization state to separate file
    Since the user init file is managed by Nix and is presented to Emacs
    as read-only, the Emacs Custom facility can't write its output to that
    file. Instead, direct it to use a different mutable not managed by
    Nix.

--- nix/home/modules/emacs/.emacs.d/init.el
+++ nix/home/modules/emacs/.emacs.d/init.el
@@ -107,31 +107,8 @@
 ;; TODO: Move this out to packaegs.el or something.
 (require 'ehelp)
 (define-key global-map "\C-h" 'ehelp-command)
-;:::::::::::::::::::::::::::::::::::::::::::::::::*
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(sanityinc-tomorrow-blue))
- '(custom-safe-themes
-   '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
- '(package-selected-packages
-   '(nix-mode nix-modeline nixpkgs-fmt 0blayout abyss-theme 2048-game lsp-mode lsp-ui flycheck-tip cargo cargo-transient flycheck-rust rust-mode color-theme-sanityinc-tomorrow persistent-scratch yasnippet yaml-mode use-package tide terraform-mode smex slime-repl-ansi-color slime-company org nginx-mode monokai-theme magit-todos magit-filenotify lua-mode jsonnet-mode json-mode js2-mode inf-ruby ido-completing-read+ go-mode exec-path-from-shell elvish-mode dired-subtree company-nginx color-theme-sanityinc-solarized color-theme-modern boxquote bbdb bazel auctex))
- '(server-done-hook (lambda nil (delete-frame)))
- '(server-switch-hook
-   (lambda nil
-     (let
-         ((server-buf
-           (current-buffer)))
-       (bury-buffer)
-       (switch-to-buffer-other-frame server-buf)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((((class color)) (:family "DejaVu Sans Mono"))))
- '(eldoc-highlight-function-argument ((((class color)) (:foreground "tomato" :bold t))))
- '(shell-output-face ((((class color) (background dark)) (:italic nil :foreground "gray90")) (((class color) (background light)) (:italic nil :foreground "darkblue"))) t)
- '(shell-prompt-face ((((class color) (background dark)) (:bold t :foreground "palegreen")) (((class color) (background light)) (:bold t :foreground "mediumslateblue"))) t))
+
+;; Preclude Emacs from writing into this file when saving
+;; customizations.
+(setq custom-file (expand-file-name "custom.el" (file-name-directory user-init-file)))
+(load custom-file t) ; Tolerate the file not existing.
--- nix/home/modules/emacs/.emacs.d/server.el
+++ nix/home/modules/emacs/.emacs.d/server.el
@@ -1,14 +1,7 @@
 ;:* server.el
 ;:*=======================
 ;; See http://www.emacswiki.org/cgi-bin/wiki/EmacsClient#toc24.
-(custom-set-variables
- '(server-switch-hook
-   (lambda ()
-     (let ((server-buf (current-buffer)))
-       (bury-buffer)
-       (switch-to-buffer-other-frame server-buf))))
- '(server-done-hook
-   (lambda ()
-     (delete-frame))))
+(setq server-window 'switch-to-buffer-other-frame)
+(add-hook 'server-done-hook 'delete-frame)
 ;:::::::::::::::::::::::::::::::::::::::::::::::::*
 (message "server settings initialized")
