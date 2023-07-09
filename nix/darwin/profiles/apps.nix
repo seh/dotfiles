267fdb080fcbd5424204f7db465d0c02b7125d3e
Steven E. Harris <seh@panix.com>
2023-07-09 12:10:11 -0400

    Include Homebrew-related configuration

--- nix/darwin/profiles/apps.nix
+++ nix/darwin/profiles/apps.nix
@@ -7,6 +7,35 @@
     lib.mkEnableOption "essential apps for Macs";
 
   config = lib.mkIf config.dotfiles.profiles.apps.enable {
-    # TODO(seh): Adapt configuration for Homebrew.
+    # Manage Homebrew with nix-darwin. Mainly useful for managing casks and
+    # App Store installations. Any formula or cask not specified in the config
+    # would be automatically uninstalled by default.
+    #
+    # See
+    # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.enable
+    # for the pertinent attributes.
+    homebrew = {
+      enable = lib.mkDefault true;
+      onActivation = {
+        autoUpdate = lib.mkDefault true;
+        upgrade = lib.mkDefault true;
+        cleanup = lib.mkDefault "uninstall";
+      };
+
+      taps = [
+        "homebrew/cask"
+      ];
+
+      casks = [
+        # TODO(seh): Once the GUI becomes viable to install via Nix,
+        # remove this in favor of that.
+        "1password"
+        "hammerspoon"
+        "slack"
+      ];
+
+      brews = [
+      ];
+    };
   };
 }
