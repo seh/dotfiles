93769ac1947b4ea1906ce5f86f13ff3150c4a6b5
Steven E. Harris <seh@panix.com>
2025-04-19 10:57:41 -0400

    Configure "jujutsu" to use "ediff" differently
    Nix's Home Manager can configure Emac's "ediff" program as a merge
    tool automatically, but when it does so, it has the "jj" tool invoke
    "emacsclient" with its "-c" or "--create-frame" flag active, such that
    "emacsclient" has Emacs create a new frame—giving it focus—and then
    blocks until the session it started completes. This makes it difficult
    to work with other existing Emacs windows, and behaves poorly when one
    quits "ediff" without saving its output deliberately, wishing to abort
    the attempted resolution.
    
    Instead, define an alternative but similar merge tool, invoking
    "ediff" without creating a fresh frame, operating asynchronously,
    allowing the session to proceed without "ediff" blocking in wait of
    its conclusion. Use the "fswatch" tool to block waiting on deletion of
    a sentinel file that indicates that the "ediff" session is complete.

--- nix/home/modules/emacs/.emacs.d/lsp.el
+++ nix/home/modules/emacs/.emacs.d/lsp.el
@@ -15,11 +15,15 @@
                   ;lsp-go-env `((GOPACKAGESDRIVER . ,(expand-file-name "gopackagesdriver")))
                   )
             (lsp-register-custom-settings
-             '(("gopls.completeUnimported" t t)
-               ;("gopls.usePlaceholders" t t)
-               ("gopls.staticcheck" t t)))
-            ;; NB: This is called "lsp-file-watch-ignored-directories"
-            ;; in newer versions of lsp-mode.
+             '(("gopls.staticcheck" t t)))
+            ;; NB: I'd prefer to catch directory names more like
+            ;; \"/var/folders/mn/cj3m9hf50sd994zln_s_fym40000gn/T/jj-resolve-q0MG7E\",
+            ;; but lsp-mode doesn't match these entries as regular expressions.
+            ;;
+            ;; Basis of inspiration: https://github.com/emacs-lsp/lsp-mode/discussions/4387#discussioncomment-8878188
+            ;; TODO(seh): Figure out how to block use of these directories again.
+            ;(cl-pushnew (getenv "TMPDIR") (lsp-session-folders-blocklist (lsp-session))
+            ;            :test 'string=)
             (dolist (p (list "[/\\\\]bazel-\\w+$"
                              (format "/private/var/tmp/_bazel_%s/" (user-login-name))
                              ;; This one didn't seem to work, even
