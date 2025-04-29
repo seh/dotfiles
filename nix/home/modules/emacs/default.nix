28a905fc1802b71335d39c926fecc8c82e0cac0b
Steven E. Harris <seh@panix.com>
2025-04-29 08:40:26 -0400

    Update Nix flake inputs

--- nix/home/modules/emacs/default.nix
+++ nix/home/modules/emacs/default.nix
@@ -24,11 +24,7 @@ in
       # C-M-SPC key binding for "mark-sexy" gets intercepted by macOS
       # and presents the Character Viewer applet.
       #package = lib.mkDefault (if isDarwin then pkgs.emacs-macport else pkgs.emacs);
-      # TODO(seh): Revert this once issue 395169 is resolved.
-      # https://github.com/NixOS/nixpkgs/issues/395169
-      #package = lib.mkDefault pkgs.emacs;
-      # Basis of inspiration: https://github.com/NixOS/nixpkgs/issues/395169#issuecomment-2797849764
-      package = pkgs.emacs.override { withNativeCompilation = false; };
+      package = lib.mkDefault pkgs.emacs;
       # TODO(seh): Should we add anything here?
       #extraConfig = ''
       #'';
