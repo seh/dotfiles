b6efca2ac1086c1dc9a7f939d1e917c60ac0222e
Steven E. Harris <seh@panix.com>
2025-05-14 12:23:08 -0400

    Use Home Manager to configure nushell
    Include several nushell plugins that are available for the platforms I
    expect to use for now.

---
+++ nix/home/modules/nushell.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.dotfiles.jujutsu;
+in
+{
+  options.dotfiles.nushell = {
+    enable = lib.mkEnableOption "nushell";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.nushell = {
+      enable = lib.mkDefault true;
+      package = lib.mkDefault pkgs.nushell;
+      plugins = with pkgs.nushellPlugins; [
+        formats
+        highlight
+        net
+        polars
+        query
+        #semver # TODO(seh): This one is not available for Darwin atop x86-64 for now.
+        units
+      ];
+    };
+    home.packages = with pkgs; [
+      nufmt
+    ];
+  };
+}
