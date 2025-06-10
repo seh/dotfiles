8ef9e02f6110a1e41466f50deeb840c6eb6918f7
Steven E. Harris <seh@panix.com>
2025-06-10 14:58:57 -0400

    Use general name for local "nix-darwin" config.
    Rather than defining top-level "darwinConfigurations" entries with
    names that match machine hostnames, instead define just one with the
    name "local", so that we can better avoid mentioning specific
    hostnames in the source code.

--- nix/nix-darwin/machines/default.nix
+++ nix/nix-darwin/machines/default.nix
@@ -5,6 +5,9 @@ let
 in
 {
   # By default nix-darwin will look for a configuration whose name
-  # matches its hostname.
-  Spinner = importDarwin ./basic.nix { };
+  # matches its hostname, per the value reported by invoking the
+  # "scutil --get LocalHostName" command.
+  #
+  # We can use a general name here to establish the common case.
+  local = importDarwin ./basic.nix { };
 }
