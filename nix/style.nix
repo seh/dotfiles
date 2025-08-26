00b8c4e3fe47d99719dba053fc8eeecc19664bbd
Steven E. Harris <seh@panix.com>
2025-08-26 14:11:26 -0400

    Augment the set of checks enforced by "pre-commit"

--- nix/style.nix
+++ nix/style.nix
@@ -62,7 +62,20 @@
 
     pre-commit.settings.hooks = {
       check-executables-have-shebangs.enable = lib.mkDefault true;
+      check-json.enable = lib.mkDefault true;
+      check-merge-conflicts.enable = lib.mkDefault true;
+      check-shebang-scripts-are-executable.enable = lib.mkDefault true;
+      check-symlinks.enable = lib.mkDefault true;
+      check-toml.enable = lib.mkDefault true;
+      check-yaml.enable = lib.mkDefault true;
+      deadnix.enable = lib.mkDefault true;
+      detect-private-keys.enable = lib.mkDefault true;
+      end-of-file-fixer.enable = lib.mkDefault true;
+      mixed-line-endings.enable = lib.mkDefault true;
+      pretty-format-json.enable = lib.mkDefault true;
+      statix.enable = lib.mkDefault true;
       treefmt.enable = lib.mkDefault true;
+      trim-trailing-whitespace.enable = lib.mkDefault true;
     };
   };
 }
