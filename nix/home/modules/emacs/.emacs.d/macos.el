ec8c389e75b49c74ea25785593d0229f1b7a2452
Steven E. Harris <seh@panix.com>
2025-08-27 09:13:12 -0400

    Define Emacs function to toggle Option key binding
    On some keyboards, I prefer to bind the Option (⌥) key to Emacs's
    "super" modifier, but on MacBook Air and MacBook Pro keyboards where
    I'm more likely to press that key instead of the adjacent Command (⌘)
    key, it's more convenient to have Option serve as "meta" as well.

--- nix/home/modules/emacs/.emacs.d/macos.el
+++ nix/home/modules/emacs/.emacs.d/macos.el
@@ -1,12 +1,27 @@
 ;:* macos.el
 ;:*=======================
 
-;; I'm not ready to accept the separation of the Emacs kill ring and
-;; the system clipboard.
-;; `osxkeys' sets this to nil by default. Force its value back.
-(setq-default select-enable-clipboard t)
 (setq ns-command-modifier 'meta  ;; Aliased by `mac-command-modifier'
-      ns-option-modifier 'super) ;; Aliased by `mac-option-modifier'
+      ns-option-modifier 'super) ;; Aliased by `mac-option-modifier', for `ns-alternate-modifier'
+
+(defun toggle-macbook-keyboard-bindings (&optional force)
+  "Toggle the bindings of ancillary keys for use with macOS, accommodating
+preferences for MacBook Air- and MacBook Pro-style keyboards.
+
+With a prefix argument, bind the keys even if they're not currently
+bound as expected."
+  (interactive "p")
+  (let ((next-binding (cl-case ns-option-modifier
+                        ('super 'meta)
+                        ('meta 'super)
+                        (t (when force
+                             'super)))))
+    (if next-binding
+        (progn
+          (setq ns-option-modifier next-binding)
+          (message "Option (⌥) key is now bound to %s." next-binding))
+      (message "Option (⌥) key is currently %s; not toggling."
+               (or ns-option-modifier "unbound")))))
 
 ;; TODO(seh): Port aquamacs-page-down-extend-region and aquamacs-page-up-extend-region?
 ;; See: https://github.com/aquamacs-emacs/aquamacs-emacs/blob/7af9e2a42348c8bc685d504834600b1e26e29a1e/aquamacs/src/site-lisp/aquamacs-editing.el#L234-L242.
