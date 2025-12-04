307049761f1dbc4d7903b9a6a20686d8e2bbbc09
Steven E. Harris <seh@panix.com>
2025-12-04 11:42:25 -0500

    Configure the "delta" tool for use with "jjui"
    Stop using Home Manager's offered integration for "delta" with
    Jujutsu, and instead configure "delta" as a Jujutsu merge tool with
    two different variants—one that displays diffs side-by-side, and the
    other in unified format. Configure Jujutsu to use the former variant
    as its diff formatter directly, rather than configuring merely "delta"
    as the pager through which to run diffs.
    
    Teach the "jjui" tool to use "delta" to display diffs in its preview
    pane as well, using the latter unified variant.

--- nix/home/modules/delta.nix
+++ nix/home/modules/delta.nix
@@ -18,9 +18,25 @@ in
       enable = lib.mkDefault true;
       package = lib.mkDefault pkgs.delta;
       enableGitIntegration = true;
-      enableJujutsuIntegration = true;
+      # NB: Rather than using the "delta" tool solely as a pager for
+      # Git-style diffs, have Jujutsu invoke "delta" to prepare the
+      # diff from the competing source files itself.
+      enableJujutsuIntegration = false;
       options = {
-        features = "navigate";
+        # Delta "Features", or named groups of options that we can
+        # activate together.
+        # Basis of inspiration: https://github.com/idursun/jjui/issues/314#issuecomment-3367379291
+        non-split-view = {
+          side-by-side = false;
+        };
+        split-view = {
+          side-by-side = true;
+        };
+        # Other non-feature options
+        features = "split-view";
+        hyperlinks = true;
+        line-numbers = true;
+        navigate = true;
       };
     };
   };
--- nix/home/modules/jujutsu.nix
+++ nix/home/modules/jujutsu.nix
@@ -22,131 +22,198 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.jujutsu = {
-      enable = lib.mkDefault true;
-      package = lib.mkDefault pkgs.jujutsu;
-      ediff = false; # We'll configure our own variant later.
-      # See https://github.com/martinvonz/jj/blob/main/docs/config.md#configuration.
-      settings = lib.mkMerge [
-        {
-          # See https://jj-vcs.github.io/jj/latest/config/#json-schema-support.
-          "$schema" = "https://jj-vcs.github.io/jj/latest/config-schema.json";
-        }
-        cfg.extraSettings
-        {
-          fsmonitor = {
-            backend = "watchman";
-            watchman = {
-              register-snapshot-trigger = true;
-            };
-          };
-          git = {
-            push-new-bookmarks = true;
-          };
-          revset-aliases = {
-            "lagging_bookmarks" = ''
-              ::bookmarks()
-              & mutable()
-              & mine()
-              ~ trunk()::
-            '';
-          };
-          template-aliases = {
-            # Basis of inspiration:
-            #   https://github.com/martinvonz/jj/blob/main/docs/config.md#display-of-commit-and-change-ids
-            #   https://v5.chriskrycho.com/essays/jj-init/#revisions-and-revsets.
-            "format_short_id(id)" = "id.shortest(12)";
-            "format_short_signature(signature)" = "signature";
-            "format_timestamp(timestamp)" = "timestamp.ago()";
-
-            "boxquoted(body, title)" = ''
-              surround(
-                "\n,----" ++ if(title, "[ " ++ title ++ " ]\n", "\n"),
-                "`----\n",
-                indent(
-                  "| ",
-                  body))
-            '';
-          };
-          templates = {
-            log = ''
-              builtin_log_compact ++
-              if(!empty &&
-                 (!description ||
-                  description.starts_with("WIP:") ||
-                  current_working_copy),
-                concat(
-                  boxquoted(diff.summary(), "summary"),
-                  boxquoted(diff.stat(80), "stat")))
-            '';
-          };
-          user = {
-            name = userConfig.fullName;
-            inherit (userConfig) email;
-          };
-        }
-        (lib.optionalAttrs hasGPGSigningKey {
-          git = {
-            # NB: Opt for this instead of enabling "signing.sign-all".
-            sign-on-push = true;
-          };
-          signing = {
-            backend = "gpg";
-            key = userConfig.gpgKey;
-
-            backends = {
-              gpg = {
-                allow-expired-keys = false;
+  config = lib.mkIf cfg.enable (
+    let
+      deltaNarrowMergeToolName = "delta-unified-view";
+    in
+    {
+      programs = {
+        jujutsu = {
+          enable = lib.mkDefault true;
+          package = lib.mkDefault pkgs.jujutsu;
+          ediff = false; # We'll configure our own variant later.
+          # See https://github.com/martinvonz/jj/blob/main/docs/config.md#configuration.
+          settings = lib.mkMerge [
+            {
+              # See https://jj-vcs.github.io/jj/latest/config/#json-schema-support.
+              "$schema" = "https://jj-vcs.github.io/jj/latest/config-schema.json";
+            }
+            cfg.extraSettings
+            {
+              fsmonitor = {
+                backend = "watchman";
+                watchman = {
+                  register-snapshot-trigger = true;
+                };
               };
-            };
-          };
-        })
-        (
-          let
-            mergeToolName = "ediff-alt";
-          in
-          {
-            merge-tools.${mergeToolName} =
+              git = {
+                push-new-bookmarks = true;
+              };
+              revset-aliases = {
+                "lagging_bookmarks" = ''
+                  ::bookmarks()
+                  & mutable()
+                  & mine()
+                  ~ trunk()::
+                '';
+              };
+              template-aliases = {
+                # Basis of inspiration:
+                #   https://github.com/martinvonz/jj/blob/main/docs/config.md#display-of-commit-and-change-ids
+                #   https://v5.chriskrycho.com/essays/jj-init/#revisions-and-revsets.
+                "format_short_id(id)" = "id.shortest(12)";
+                "format_short_signature(signature)" = "signature";
+                "format_timestamp(timestamp)" = "timestamp.ago()";
+
+                "boxquoted(body, title)" = ''
+                  surround(
+                    "\n,----" ++ if(title, "[ " ++ title ++ " ]\n", "\n"),
+                    "`----\n",
+                    indent(
+                      "| ",
+                      body))
+                '';
+              };
+              templates = {
+                log = ''
+                  builtin_log_compact ++
+                  if(!empty &&
+                     (!description ||
+                      description.starts_with("WIP:") ||
+                      current_working_copy),
+                    concat(
+                      boxquoted(diff.summary(), "summary"),
+                      boxquoted(diff.stat(80), "stat")))
+                '';
+              };
+              user = {
+                name = userConfig.fullName;
+                inherit (userConfig) email;
+              };
+            }
+            (lib.optionalAttrs hasGPGSigningKey {
+              git = {
+                # NB: Opt for this instead of enabling "signing.sign-all".
+                sign-on-push = true;
+              };
+              signing = {
+                backend = "gpg";
+                key = userConfig.gpgKey;
+
+                backends = {
+                  gpg = {
+                    allow-expired-keys = false;
+                  };
+                };
+              };
+            })
+            (
               let
-                programName = "emacs-ediff-alt";
-                emacsDiffProgram = pkgs.writeShellScriptBin programName (
-                  builtins.readFile (./. + "/${programName}")
-                );
+                deltaMergeToolName = "delta-split-view";
+                # NB: Assume that "delta" is not wrapped.
+                deltaProgram = lib.mkIf config.dotfiles.delta.enable (lib.getExe config.programs.delta.package);
+                emacsMergeToolName = "ediff-alt";
               in
               {
-                program = lib.getExe emacsDiffProgram;
-                merge-args = [
-                  "$left"
-                  "$right"
-                  "$base"
-                  "$output"
-                ];
-                # Attempt to detect when we exit ediff-merge without
-                # resolving all the conflicts, leaving some still
-                # present in the output file.
-                merge-tool-edits-conflict-markers = true;
-              };
-            ui = {
-              editor =
-                let
-                  programName = "emacsclient-for-jj-describe";
-                  emacsclientProgram = pkgs.writeShellScriptBin programName (
-                    builtins.readFile (./. + "/${programName}")
-                  );
-                in
-                lib.getExe emacsclientProgram;
-              log-word-wrap = true;
-              merge-editor = mergeToolName;
-              show-cryptographic-signatures = true;
+                merge-tools = {
+                  # Basis of inspiration: https://github.com/idursun/jjui/issues/314#issuecomment-3367379291
+                  ${deltaNarrowMergeToolName} = lib.mkIf config.dotfiles.delta.enable {
+                    program = deltaProgram;
+                    diff-args = [
+                      "--width"
+                      "$width"
+                      "--features"
+                      "non-split-view"
+                      "$left"
+                      "$right"
+                    ];
+                  };
+                  ${deltaMergeToolName} = lib.mkIf config.dotfiles.delta.enable {
+                    program = deltaProgram;
+                    diff-args = [
+                      "--width"
+                      "$width"
+                      "--features"
+                      "split-view"
+                      "$left"
+                      "$right"
+                    ];
+                  };
+                  ${emacsMergeToolName} =
+                    let
+                      programName = "emacs-ediff-alt";
+                      emacsDiffProgram = pkgs.writeShellScriptBin programName (
+                        builtins.readFile (./. + "/${programName}")
+                      );
+                    in
+                    {
+                      program = lib.getExe emacsDiffProgram;
+                      merge-args = [
+                        "$left"
+                        "$right"
+                        "$base"
+                        "$output"
+                      ];
+                      # Attempt to detect when we exit ediff-merge without
+                      # resolving all the conflicts, leaving some still
+                      # present in the output file.
+                      merge-tool-edits-conflict-markers = true;
+                    };
+                };
+                ui = {
+                  diff-formatter = deltaMergeToolName;
+                  editor =
+                    let
+                      programName = "emacsclient-for-jj-describe";
+                      emacsclientProgram = pkgs.writeShellScriptBin programName (
+                        builtins.readFile (./. + "/${programName}")
+                      );
+                    in
+                    lib.getExe emacsclientProgram;
+                  log-word-wrap = true;
+                  merge-editor = emacsMergeToolName;
+                  show-cryptographic-signatures = true;
+                };
+              }
+            )
+          ];
+        };
+        jjui = {
+          enable = lib.mkDefault true;
+          package = lib.mkDefault pkgs.jjui;
+          settings = {
+            preview = {
+              file_command = [
+                "diff"
+                "--color"
+                "always"
+                "-r"
+                "$change_id"
+                "--config"
+                "ui.diff-formatter=${deltaNarrowMergeToolName}"
+                "$file"
+              ];
+              revision_command = [
+                "show"
+                "--color"
+                "always"
+                "-r"
+                "$change_id"
+                "--config"
+                "ui.diff-formatter=${deltaNarrowMergeToolName}"
+              ];
             };
-          }
-        )
+            ui = {
+              tracer = {
+                enabled = true;
+              };
+            };
+          };
+        };
+      };
+      home.packages = with pkgs; [
+        jj-fzf
       ];
-    };
-    home.packages = with pkgs; [
-      jj-fzf
-      jjui
-    ];
-  };
+    }
+  );
 }
