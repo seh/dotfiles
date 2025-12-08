e8724d92156b10117f899dd3ff204ecd0a458d9f
Steven E. Harris <seh@panix.com>
2025-12-08 09:28:00 -0500

    template: Rely on caches as Nix store substituters

--- nix/templates/single/flake.nix
+++ nix/templates/single/flake.nix
@@ -7,6 +7,17 @@
     nixpkgs.follows = "dotfiles/nixpkgs";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs =
     { self, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } (
