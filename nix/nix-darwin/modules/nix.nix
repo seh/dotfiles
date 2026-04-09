e6a5661bd5ca7cf55ac5aa8f1ce69b5d0173ec4f
Steven E. Harris <seh@panix.com>
2026-04-09 10:48:15 -0400

    Set experimental-features in per-platform modules
    Replace the free-form "nix.extraOptions" string in the
    "nix/nix-darwin/modules/nix.nix" file with the structured
    "nix.settings.experimental-features" list.
    
    Move the same setting out of the inline "machineDefaultsModule" in the
    "nix/lib/flake-module.nix" file and into the
    "nix/nixos/modules/nix.nix" file, so both platforms configure
    experimental features similarly in their respective "nix.nix" module
    files rather than inline in the "mkDarwin" and "mkNixOS" functions.

--- nix/nix-darwin/modules/nix.nix
+++ nix/nix-darwin/modules/nix.nix
@@ -5,9 +5,11 @@
   pkgs,
   config,
   ...
-}: {
+}:
+{
   nix.package = lib.mkDefault pkgs.lixPackageSets.${config.dotfiles.flakeOptions.lix.channel}.lix;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 }
--- nix/nixos/modules/nix.nix
+++ nix/nixos/modules/nix.nix
@@ -3,6 +3,11 @@
   pkgs,
   config,
   ...
-}: {
+}:
+{
   nix.package = lib.mkDefault pkgs.lixPackageSets.${config.dotfiles.flakeOptions.lix.channel}.lix;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 }
