1519186a7102b73a1b7edb1b2bbcafb81383fdd9
Steven E. Harris <seh@panix.com>
2026-05-28 13:07:04 -0400

    Define "trusted-users" to honor flake substituters
    Allow more user accounts to make use of "extra-substituters" when
    fetching Nix inputs, especially useful for consuming hosted caches
    when building the flake's outputs.

--- modules/nixos/features/nix.nix
+++ modules/nixos/features/nix.nix
@@ -7,9 +7,13 @@ flakeLib.mkFeature "nix" {
     ...
   }: {
     nix.package = lib.mkDefault pkgs.lixPackageSets.${config.dotfiles.lix.channel}.lix;
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+    nix.settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      # NB: NixOS includes the "root" user automatically.
+      trusted-users = builtins.attrNames config.dotfiles.users;
+    };
   };
 }
