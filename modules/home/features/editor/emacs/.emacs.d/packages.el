bb9e009c88e4c304f346c5ad948faaa1f62f9883
Steven E. Harris <seh@panix.com>
2026-09-09 17:06:16 -0400

    Skip Magit's diff for a Jujutsu description
    Magit shows a diff of the working tree whenever a commit message
    opens, but Git's view of a Jujutsu working tree need not resemble the
    change being described, and can be large enough to leave Emacs busy
    for minutes.

--- modules/home/features/editor/emacs/.emacs.d/packages.el
+++ modules/home/features/editor/emacs/.emacs.d/packages.el
@@ -175,7 +175,20 @@
 ;:*=======================
 ;:* magit
 (use-package magit
+  :preface
+  (defun seh-git-commit-skip-jujutsu-diff ()
+    "Stop Magit from diffing the working tree for a Jujutsu description.
+Magit shows a diff of the working tree whenever it recognizes a commit
+message; for a Jujutsu description, that diff is Git's view of a
+Jujutsu working tree, which need not resemble the change being
+described and can be huge."
+    (when (and buffer-file-name
+               (string-suffix-p ".jjdescription" buffer-file-name))
+      (setq-local magit-commit-show-diff nil)))
   :if (seh-activation-name-in-effect-p "vcs/git")
+  :init
+  (when (seh-activation-name-in-effect-p "vcs/jujutsu")
+    (add-hook 'git-commit-setup-hook #'seh-git-commit-skip-jujutsu-diff))
   :config
   (setq magit-completing-read-function #'ivy-completing-read
         ;; Edit jj commit messages using "git-commit-mode":
